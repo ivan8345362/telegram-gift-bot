504015d6b3531249ffa1a444893037e90523236c
ivan8345362 <pomorcev555@mail.ru>
2025-12-09 23:47:09 +0700

    Commit-5

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("369792660", "0"))  # ID админа из переменных окружения
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # ID админа из переменных окружения
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
