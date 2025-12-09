fa5404d5a7c00119015991c71f110eb8e4d78173
ivan8345362 <pomorcev555@mail.ru>
2025-12-09 23:29:57 +0700

    Commit-2

--- bot.py
+++ bot.py
@@ -3,26 +3,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
 # ВСТАВЬ СВОЙ РЕАЛЬНЫЙ ТОКЕН СЮДА !!!
-BOT_TOKEN = "import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
-
-# ВСТАВЬ СВОЙ РЕАЛЬНЫЙ ТОКЕН СЮДА !!!
-BOT_TOKEN = "1234567890:ABCdefGhIJklMNopQRSTuvWXyz"
-
-# Пароль для входа в админку
-ADMIN_PASSWORD = "1234"
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.answer("Бот работает!")
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
-"
+BOT_TOKEN = "8559685531:AAFaR0iLEZtBDCu6qSPlla_LANLJmCK5awk"
 
 # Пароль для входа в админку
 ADMIN_PASSWORD = "1234"
