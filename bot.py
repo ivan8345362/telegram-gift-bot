8ce1518535ed39db631d0f78d4803cc259a728d3
ivan8345362 <pomorcev555@mail.ru>
2025-12-09 22:38:07 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,1 @@
+import osfrom aiogram import Bot, Dispatcher, typesfrom aiogram.utils import executorBOT_TOKEN = os.environ.get("BOT_TOKEN")bot = Bot(token=BOT_TOKEN)dp = Dispatcher(bot)@dp.message_handler(commands=['start'])async def start(message: types.Message):    await message.answer("Бот работает!")if __name__ == "__main__":    executor.start_polling(dp, skip_updates=True)
